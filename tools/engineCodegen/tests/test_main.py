d16cc7ae00a8e0562096a96fc6a2c36d17caf4ff
RuslanZhuch <ruslanacs93@mail.ru>
2023-07-11 18:48:53 +0200

    Removing dest files before test

--- tools/engineCodegen/tests/test_main.py
+++ tools/engineCodegen/tests/test_main.py
@@ -1,5 +1,11 @@
 import unittest
+from pathlib import Path
+
+def _remove_dest_files():
+    [f.unlink() for f in Path("dest").glob("*") if f.is_file()] 
 
 if __name__ == '__main__':
+    _remove_dest_files()
+    
     testsuite = unittest.TestLoader().discover('.')
     unittest.TextTestRunner(verbosity=1).run(testsuite)
